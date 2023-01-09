1894f85252ff53958573a605a451037d1388998d
wadaudzai <wadaudzai@gmail.com>
2023-01-09 02:51:45 -0500

    Create the first version of TodoList.

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import TodoContainer from './components/TodoContainer';
+
+function App() {
+  return (
+    <React.StrictMode>
+      <TodoContainer />
+    </React.StrictMode>
+  );
+}
+
+export default App;
