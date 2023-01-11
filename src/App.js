2b4ffa209cd585b68147a3ad6e8c5ed58d50f712
wadaudzai <wadaudzai@gmail.com>
2023-01-10 21:03:22 -0500

    Try State successfully

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import TodoContainer from './components/TodoContainer';
+import TryState from './components/TryState';
 
 function App() {
   return (
     <React.StrictMode>
       <TodoContainer />
+      <TryState />
     </React.StrictMode>
   );
 }
